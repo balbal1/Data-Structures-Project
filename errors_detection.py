6102cdf8320c702e05e4aff444a069abcdb3bcb1
Rana <89163034+RanaMohamad21@users.noreply.github.com>
2023-12-16 09:37:51 +0200

    Update errors_detection.py

--- errors_detection.py
+++ errors_detection.py
@@ -1,4 +1,4 @@
-
+import re
 def error_detection(text):
     xml_file = text
     filter = r'<(/?\w+)>'
@@ -7,36 +7,36 @@ def error_detection(text):
     tags = [] # line: , status: correct/missing tag
 
     def updateTags(line,status,name):
-        tag_info = {}
-        tag_info['line'] = line
-        tag_info['status'] = status
-        tag_info['name'] = name
+        tag_info = []
+        tag_info[0] = line
+        tag_info[1] = status
+        tag_info[2] = name
         tags.append(tag_info)
 
     # Get the tags and its corresponding line
     for i in range(len(xml_file)): 
         matches=re.finditer(filter,xml_file[i])
         for match in matches:
-            tag_info = {}
-            tag_info['line'] = i + 1
-            tag_info['tag'] = match.group(1)
+            tag_info = []
+            tag_info[0] = i + 1
+            tag_info[1] = match.group(1)
             tags_list.append(tag_info)
             
     # Detect errors
     for tag in tags_list:
-        if not tag['tag'].startswith('/'): # open Tag
+        if not tag[1].startswith('/'): # open Tag
             openStack.append(tag)
-        elif tag['tag'].startswith('/'): # close Tag
-            closeTag = tag['tag'][1:]
-            tag_line = tag['line']
+        elif tag[1].startswith('/'): # close Tag
+            closeTag = tag[1][1:]
+            tag_line = tag[0]
             if openStack != []:
-                if closeTag == openStack[-1]['tag']:
-                    updateTags(tag_line,'valid',openStack[-1]['tag'])
+                if closeTag == openStack[-1][1]:
+                    updateTags(tag_line,'valid',openStack[-1][1])
                     openStack.pop()
-                elif len(openStack) > 1 and closeTag == openStack[-2]['tag']:
-                    updateTags(openStack[-1]['line'],'Missing Close tag',openStack[-1]['tag'])
+                elif len(openStack) > 1 and closeTag == openStack[-2][1]:
+                    updateTags(openStack[-1][0],'Missing Close tag',openStack[-1][1])
                     openStack.pop()
-                    updateTags(tag_line, 'Valid',openStack[-1]['tag'])
+                    updateTags(tag_line, 'Valid',openStack[-1][1])
                     openStack.pop()
                 else:
                     updateTags(tag_line,'Missing open tag',closeTag)
@@ -48,7 +48,7 @@ def error_detection(text):
 
     if openStack != []:
         while openStack != []:
-            updateTags(openStack[-1]['line'],'Missing Close tag',openStack[-1]['tag'])
+            updateTags(openStack[-1][0],'Missing Close tag',openStack[-1][1])
             openStack.pop()
     print(tags)
         
