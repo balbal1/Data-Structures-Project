c1f879d8ce2021fcdd07b38938eca668266ede04
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2023-12-21 16:44:03 +0200

    Update parse_xml.py

--- parse_xml.py
+++ parse_xml.py
@@ -1,47 +1,56 @@
 import xml.etree.ElementTree as ET
 
-def parse_xml(file_path):
+class Node:
+    def __init__(self, name="", text="", children=None, level=0):
+        self.name = name
+        self.text = text
+        self.children = children if children is not None else []
+        self.level = level
+
+    def minify(self):
+        if not self.children:
+            return f"<{self.name}>{self.text}</{self.name}>"
+        string = f"<{self.name}>"
+        for child in self.children:
+            string += child.minify()
+        string += f"</{self.name}>"
+        return string
+
+    def print_tree(self):
+        for line in self.stringify():
+            print(line)
+
+    def stringify(self):
+        if not self.children:
+            return ["  " * self.level + f"<{self.name}>{self.text}</{self.name}>"]
+        string = [f"  " * self.level + f"<{self.name}>"]
+        for child in self.children:
+            string.extend(child.stringify())
+        string.append(f"  " * self.level + f"</{self.name}>")
+        return string
+
+def build_tree_from_xml(xml_element, level=0):
+    node = Node()
+    node.name = xml_element.tag
+    node.text = xml_element.text.strip() if xml_element.text else ""
+    node.level = level
+
+    for child_elem in xml_element:
+        child_node = build_tree_from_xml(child_elem, level + 1)
+        node.children.append(child_node)
+
+    return node
+
+def parse_xml_to_tree(file_path):
     tree = ET.parse(file_path)
     root = tree.getroot()
 
-    users = []
+    return build_tree_from_xml(root)
 
-    for user_elem in root.findall('user'):
-        user = {
-            'id': user_elem.find('id').text,
-            'name': user_elem.find('name').text,
-            'posts': [],
-            'followers': []
-        }
-
-        for post_elem in user_elem.findall('posts/post'):
-            post = {
-                'body': post_elem.find('body').text,
-                'topics': [topic.text.strip() for topic in post_elem.findall('topics/topic')]
-            }
-            user['posts'].append(post)
-
-        for follower_elem in user_elem.findall('followers/follower'):
-            follower_id = follower_elem.find('id').text
-            user['followers'].append({'id': follower_id})
-
-        users.append(user)
-
-    return users
-
+# Example test:
 if __name__ == "__main__":
     file_path = "sample.xml"
-    parsed_data = parse_xml(file_path)
+    parsed_tree = parse_xml_to_tree(file_path)
 
-    # Print the parsed data in a structured format
-    for user in parsed_data:
-        print(f"User ID: {user['id']}")
-        print(f"User Name: {user['name']}")
-        print("Posts:")
-        for post in user['posts']:
-            print(f"  - Body: {post['body']}")
-            print(f"    Topics: {', '.join(post['topics'])}")
-        print("Followers:")
-        for follower in user['followers']:
-            print(f"  - Follower ID: {follower['id']}")
-        print("\n")
+    # Print the parsed tree
+    parsed_tree.print_tree()
