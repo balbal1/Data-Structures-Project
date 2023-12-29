57f4ea76ff4ec51dd4ab8f36005ce60d634676bb
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2023-12-29 18:15:55 +0200

    Update undo_redo.py

--- undo_redo.py
+++ undo_redo.py
@@ -13,29 +13,30 @@ def prettifyHandle(self):
         self.outputTextArea.insertPlainText(self.tree.convert(False, True))
         self.save_current_state()
                 
-def save_current_state(self):
+        def save_current_state(self):
             current_content = self.outputTextArea.toPlainText()
             self.stack.append(current_content)
 
+  
+
     def undo(self):
-        if len(self.stack) > 1:
-            last_operation = self.stack.pop()
-            previous_state = self.stack[-1]
+        if self.stack:
+            current_state = self.stack.pop()
+            self.redo_stack.append(current_state)
             self.outputTextArea.clear()
-            self.outputTextArea.insertPlainText(previous_state)
-            self.redo_stack.append(self.stack.pop())
+            self.outputTextArea.insertPlainText(self.stack[-1] if self.stack else "")
         else:
             self.outputTextArea.clear()
             self.outputTextArea.insertPlainText("")
-            self.redo_stack.append("")
-
+            # self.redo_stack.append("")
 
     def redo(self):
         if self.redo_stack:
+            next_state = self.redo_stack.pop()
+            self.stack.append(next_state)
             self.outputTextArea.clear()
-            self.outputTextArea.insertPlainText(self.redo_stack.pop())
-            self.stack.append(self.redo_stack.pop())
+            self.outputTextArea.insertPlainText(next_state)
         else:
             self.outputTextArea.clear()
             self.outputTextArea.insertPlainText("")
-            self.stack.append(self.redo_stack.pop())
+            # self.stack.append("")
