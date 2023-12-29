80088df28487af4aa07189df852c35629a220923
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2023-12-29 16:58:16 +0200

    Update undo_redo.py

--- undo_redo.py
+++ undo_redo.py
@@ -1,80 +1,26 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
-        self.prev = None
-
-    def __str__(self):
-        return f"Node({self.data})"
-
-class Stack:
-    def __init__(self):
-        self.pointer = None
-
-    def push(self, x):
-        if not isinstance(x, Node):
-            x = Node(x)
-
-        if self.is_empty():
-            self.pointer = x
-        else:
-            x.next = self.pointer
-            self.pointer.prev = x
-            self.pointer = x
-
-    def pop(self):
-        if self.is_empty():
-            print(f'Stack Underflow')
-        else:
-            self.pointer = self.pointer.next
-
-    def is_empty(self):
-        return self.pointer is None
-
-    def __str__(self):
-        string = ''
-        current = self.pointer
-        while current:
-            string += f'{current.data}->'
-            current = current.next
-        if string:
-            print(f'Stack Pointer = {self.pointer.data}')
-            return f'[{string[:-2]}]'
-        return '[]'
+def save_current_state(self):
+            current_content = self.outputTextArea.toPlainText()
+            self.stack.append(current_content)
 
     def undo(self):
-        x = self.pointer
-        self.pop()
-        print(x)
+        if len(self.stack) > 1:
+            last_operation = self.stack.pop()
+            previous_state = self.stack[-1]
+            self.outputTextArea.clear()
+            self.outputTextArea.insertPlainText(previous_state)
+            self.redo_stack.append(self.stack.pop())
+        else:
+            self.outputTextArea.clear()
+            self.outputTextArea.insertPlainText("")
+            self.redo_stack.append("")
+
 
     def redo(self):
-        x = self.pointer.prev
-        if x is None:
-            print("nothing to redo")
+        if self.redo_stack:
+            self.outputTextArea.clear()
+            self.outputTextArea.insertPlainText(self.redo_stack.pop())
+            self.stack.append(self.redo_stack.pop())
         else:
-            self.push(x)
-            print(x)
-
-if __name__ == "__main__":
-    stack = Stack()
-
-    stack.push(1)
-    stack.push(2)
-    stack.push(3)
-    stack.push(4)
-    stack.push(5)
-
-    print("stack is:")
-    print(stack)
-    print()
-
-    stack.undo()
-
-    print("after undo:")
-    print(stack)
-    print()
-
-    stack.redo()
-
-    print("after redo:")
-    print(stack)
+            self.outputTextArea.clear()
+            self.outputTextArea.insertPlainText("")
+            self.stack.append(self.redo_stack.pop())
