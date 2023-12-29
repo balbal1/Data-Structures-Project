b25f9855d6c24b19e50670401a21940e02194cf0
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2023-12-29 16:58:52 +0200

    Update undo_redo.py

--- undo_redo.py
+++ undo_redo.py
@@ -1,3 +1,18 @@
+def prettifyHandle(self):
+        self.outputTextArea.clear()
+        self.outputTextArea.insertPlainText(self.tree.prettify())
+        self.save_current_state()
+
+    def minifyHandle(self):
+        self.outputTextArea.clear()
+        self.outputTextArea.insertPlainText(self.tree.minify())
+        self.save_current_state()
+
+    def convertHandle(self):
+        self.outputTextArea.clear()
+        self.outputTextArea.insertPlainText(self.tree.convert(False, True))
+        self.save_current_state()
+                
 def save_current_state(self):
             current_content = self.outputTextArea.toPlainText()
             self.stack.append(current_content)
